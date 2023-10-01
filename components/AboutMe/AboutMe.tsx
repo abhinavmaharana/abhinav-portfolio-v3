3f1f484a16f749d8005cfa4efae5ecd2a5fcc77c
abhinavmaharana <abhinavmaharana.004@gmail.com>
2023-10-01 14:21:54 +0530

    🐛 About Us Section

--- components/AboutMe/AboutMe.tsx
+++ components/AboutMe/AboutMe.tsx
@@ -8,13 +8,13 @@ function AboutMe(): JSX.Element {
   // Use the correct type for scrollY
   const { scrollY } = useScroll();
 
-  const y1 = useTransform(scrollY, [0, 5500], [0, -200], { clamp: false });
-  const y2 = useTransform(scrollY, [0, 1500], [0, -200], { clamp: false });
+  const y1 = useTransform(scrollY, [0, 400], [100, 0], { clamp: false });
+  const y2 = useTransform(scrollY, [0, 1000], [0, -200], { clamp: false });
 
   return (
     <div
       id="about"
-      className="w-full py-[50px] mt-1 md:py-[100px] bg-white text-black relative overflow-hidden"
+      className="w-full py-[50px] xl:-mb-5 md:py-[100px] bg-black  relative overflow-hidden"
     >
       {/* BACKGROUND ELEMENTS START */}
       <div className="sec-2-bg-gradient" />
@@ -34,6 +34,11 @@ function AboutMe(): JSX.Element {
           <span>A Software Developer</span>
           <span className="flex items-center gap-2">
             <span>Based</span>
+            <img
+                src='/illustrations/indian-flag.png'
+                alt=""
+                className="w-[70px] md:w-[150px] 2xl:w-[190px] block mt-1 md:mt-2"
+            />
             <span>In India</span>
           </span>
         </motion.div>
